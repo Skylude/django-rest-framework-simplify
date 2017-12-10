92c93dada2d19cba29ffa21d168db6adcb43db67
Skyler Cain <skylercain@outlook.com>
2017-12-10 00:57:04 -0700

    Renamed exported project into pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='django-restframework-simplify',
+    name='django-rest-framework-simplify',
     version='1.0.0.dev',
     description='Django Rest Framework Simplify',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
-    url='https://github.com/Skylude/django-restframework-simplify',
+    url='https://github.com/Skylude/django-rest-framework-simplify',
     packages=['rest_framework_simplify', 'rest_framework_simplify.services', 'rest_framework_simplify.services.sql_executor']
 )
