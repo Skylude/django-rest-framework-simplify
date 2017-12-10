58c02f4568dfd89771c7d135124c1b1b0137e80e
Skyler Cain <skylercain@outlook.com>
2017-12-10 00:51:31 -0700

    Changed test runner to nose

--- test_proj/settings.py
+++ test_proj/settings.py
@@ -132,3 +132,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+
+# set test runner to nose
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
