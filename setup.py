ba188137a5143937f4ac22ab413effa8515e41c5
Josh <josh@rentdynamics.com>
2018-12-27 17:08:52 -0700

    Update setup.py
    Update Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-rest-framework-simplify',
-    version='1.3.1.dev1',
+    version='1.3.2.dev1',
     description='Django Rest Framework Simplify',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
