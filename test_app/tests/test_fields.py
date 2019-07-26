fa3cee918692145b2ea2677becd0540a83567c46
Chase Gibbons <chase@rentdynamics.com>
2019-07-26 13:50:26 -0600

    added malformed json test case

--- test_app/tests/test_fields.py
+++ test_app/tests/test_fields.py
@@ -57,39 +57,80 @@ class CustomFieldTests(unittest.TestCase):
     class JsonTextFieldTests(unittest.TestCase):
 
         @patch.object(SimplifyJsonTextField, 'to_python')
-        def test_json_text_field_calls_mock_to_python(self, mock_to_python):
+        def test_json_text_field_calls_mock_to_python_when_set(self, mock_to_python):
+            # arrange
             mock_to_python.return_value = []
+            json_text = json.loads('[{ "description": "isn\'t it beautiful outside?" }]')
+
+            # act
+            jt_class = DataGenerator.set_up_json_text_field_class(
+                json_text=json_text)
+
+            # assert
+            self.assertTrue(mock_to_python.called)
+
+        @patch.object(SimplifyJsonTextField, 'from_db_value')
+        def test_json_text_field_calls_mock_from_db_value_when_retrieved_from_db(self, mock_from_db):
+            # arrange
+            mock_from_db.return_value = []
             json_text = json.loads('[{ "description": "isn\'t it beautiful outside?" }]')
             jt_class = DataGenerator.set_up_json_text_field_class(
                 json_text=json_text)
 
-            self.assertTrue(mock_to_python.called)
+            # act
+            jt_return = JsonTextFieldClass.objects.get(id=jt_class.id)
+
+            # assert
+            self.assertTrue(mock_from_db.called)
 
         def test_json_text_field_returns_json_value_as_list(self):
+            # arrange
             json_text = json.loads('{ "description": "isn\'t it beautiful outside?" }')
             jt_class = DataGenerator.set_up_json_text_field_class(
                 json_text=json_text)
 
+            # act
             jt_return = JsonTextFieldClass.objects.get(id=jt_class.id)
 
+            # assert
             self.assertIsNotNone(jt_return)
             self.assertTrue(jt_return.json_text, List)
 
         def test_json_text_field_returns_json_value_as_object(self):
+            # arrange
             json_text = json.loads('{ "description": "isn\'t it beautiful outside?" }')
             jt_class = DataGenerator.set_up_json_text_field_class(
                 json_text=json_text)
 
+            # act
             jt_return = JsonTextFieldClass.objects.get(id=jt_class.id)
 
+            # assert
             self.assertIsNotNone(jt_return)
             self.assertTrue(jt_return.json_text, dict)
 
         def test_json_text_field_returns_json_value_as_none(self):
+            # arrange
             json_text = None
             jt_class = DataGenerator.set_up_json_text_field_class(
                 json_text=json_text)
 
+            # act
             jt_return = JsonTextFieldClass.objects.get(id=jt_class.id)
 
+            # assert
             self.assertIsNone(jt_return.json_text)
+
+        def test_json_text_field_returns_string_when_json_in_database_is_malformed(self):
+            # arrange
+            json_text_in_db = '[{ \'description\':: \'isn\'t it beautiful outside?\' }]'
+
+            # act
+            jt_class = DataGenerator.set_up_json_text_field_class(
+                json_text=json_text_in_db)
+
+            jt_return = JsonTextFieldClass.objects.get(id=jt_class.id)
+
+            # assert
+            self.assertIsNotNone(jt_return.json_text)
+            self.assertTrue(isinstance(jt_return.json_text, str))
